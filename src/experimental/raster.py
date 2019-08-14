a35f2679e939352bd74d943836485761f466adf5
UBombar <ufukbombar@gmail.com>
2019-08-14 17:31:01 -0500

    Added raster experimentation

--- src/experimental/raster.py
+++ src/experimental/raster.py
@@ -1,29 +1,85 @@
 import gdal
-import numpy as np
-import osr
+import numpy as np 
+from utility import ogrutil as util
+import ogr
+
+SIZE = (1000, 1000)
+SRS = gdal.osr.SpatialReference()
+DRIVER : gdal.Driver = gdal.GetDriverByName('gtiff')
+
+SRS.ImportFromEPSG(3857)
+SRS = SRS.ExportToWkt()
+
+'''
+
+def process_data():
+    return np.ones(SIZE, dtype=np.int8) * 0
+
+
+data = process_data()
+
+driver : gdal.Driver = gdal.GetDriverByName('gtiff')
+ref = gdal.osr.SpatialReference()
+ref.ImportFromEPSG(3857)
+
+
+dataset : gdal.Dataset = driver.Create('data/raster/HELLO.tiff', *SIZE)
+dataset.SetProjection(ref.ExportToWkt())
+
+band : gdal.Band = dataset.GetRasterBand(1)
+
+
+dataset.SetGeoTransform((-9968426.1904, 2.2, 0, 4078845.3914, 0, -2.2))
+
+
+band.WriteArray(data)
+
+dataset.FlushCache()
+
+del dataset
+
+
+
+'''
+
+r = 15
+size2 = (SIZE[0] // r, SIZE[1] // r)
+
+dataset : gdal.Dataset = gdal.Open('data/raster/HELLO.tiff', gdal.GA_Update)
 
 driver : gdal.Driver = gdal.GetDriverByName('gtiff')
 
-inds : gdal.Dataset     = gdal.Open('data/raster/UTM2GTIF.tiff')
 
-outds : gdal.Dataset    = driver.Create('data/raster/TEST.tiff', 200, 200)
+dataset2 : gdal.Dataset = driver.Create('data/raster/SALUTE.tiff', *size2)
+dataset2.SetProjection(SRS)
+
+dataset2.SetGeoTransform((-9968426.1904, 2.2 * r, 0, 4078845.3914, 0, -2.2 * r))
+
+reader = util.UtilReader('data/route/bumbs.shp')
+
+lyr = reader.getLayer()
+
+for i in range(10):
+
+    lyr.SetAttributeFilter('height={}'.format(i))
+
+    gdal.RasterizeLayer(dataset, [1], lyr, burn_values=[i + 1])
+
+gdal.ReprojectImage(dataset, dataset2, None, None, gdal.GRA_Bilinear)
+
+
+
+reader.close()
+
+dataset2.FlushCache()
+dataset.FlushCache()
+del dataset
+del dataset2
+
 
 '''
-ds : gdal.Dataset = gdal.Open('data/raster/UTM2GTIF.tiff')
-trans = ds.GetGeoTransform()
-del ds
+dataset : gdal.Dataset = gdal.Open('data/raster/HELLO.tiff', gdal.GA_Update)
+memdriver : ogr.Driver = ogr.GetDriverByName('Memory')
+memdriver.CreateDataSource('')
 
-driver : gdal.Driver = gdal.GetDriver(0)
-ds = driver.Create('data/raster/TEST.geotiff', 200, 200)
-srs = osr.SpatialReference()
-
-srs.ImportFromEPSG(3857)
-ds.SetProjection(srs.ExportToWkt())
-ds.SetGeoTransform(trans)
-
-array = np.ndarray((200, 200), dtype=int)
-
-band : gdal.Band = ds.GetRasterBand(1)
-band.WriteRaster(0, 0, 200, 200)
 '''
-
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 # import utility.router as router
 # import experimental.raster as raster
 # import math
+import experimental.raster
 
 '''
     r = router.Router('data/route/crosses.shp', 'data/route/roads.shp', 'data/route/stops.shp')
@@ -11,44 +12,3 @@
     print('done')
 '''
 
-'''
-import mysql.connector as mysql
-import psycopg2 as pg
-
-
-pg.connect()
-
-'''
-
-import networkx as nx 
-import matplotlib.pyplot as plt 
-
-G = nx.DiGraph()
-
-G.add_weighted_edges_from([(0, 1, 10), (1, 2, 5), (2, 4, 7), (0, 3, 14)])
-
-
-plt.subplot(111)
-nx.draw(G, with_labels=True)
-# plt.subplot(122)
-
-# nx.draw_shell(G, nlist=[range(5, 10), range(5)], with_labels=True, font_weight='bold')
-
-plt.show()
-
-
-'''
-mydb : mysql.Connect = mysql.connect(host="localhost",user="ubombar",passwd="123123",database="test")
-
-mycursor = mydb.cursor()
-
-
-
-for i in range(4):
-	value = (i, 'Hello Everyone')
-	ret = mycursor.execute("INSERT INTO test.test VALUES(%s, %s)", value)
-	print('excecuted', ret)
-    
-mydb.commit()
-mydb.close()
-'''

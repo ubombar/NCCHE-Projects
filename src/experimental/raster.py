a908351fcf2a55088527d337f1149224bd14a3f5
UBombar <ufukbombar@gmail.com>
2019-08-14 17:55:37 -0500

    Added plotter

--- src/experimental/raster.py
+++ src/experimental/raster.py
@@ -2,6 +2,8 @@ import gdal
 import numpy as np 
 from utility import ogrutil as util
 import ogr
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
 
 SIZE = (1000, 1000)
 SRS = gdal.osr.SpatialReference()
@@ -9,6 +11,31 @@ DRIVER : gdal.Driver = gdal.GetDriverByName('gtiff')
 
 SRS.ImportFromEPSG(3857)
 SRS = SRS.ExportToWkt()
+
+
+dem = gdal.Open('data/raster/SALUTE.tiff')
+gt  = dem.GetGeoTransform()
+dem = dem.ReadAsArray()
+
+fig, ax = plt.subplots(figsize=(16,8), subplot_kw={'projection': '3d'})
+
+
+xres = gt[1]
+yres = gt[5]
+
+X = np.arange(gt[0], gt[0] + dem.shape[1]*xres, xres)
+Y = np.arange(gt[3], gt[3] + dem.shape[0]*yres, yres)
+
+X, Y = np.meshgrid(X, Y)
+
+surf = ax.plot_surface(X,Y,dem, rstride=1, cstride=1, cmap=plt.cm.RdYlBu_r, vmin=0, vmax=10, linewidth=0, antialiased=True)
+
+ax.set_zlim(0, 10) # to make it stand out less
+ax.view_init(60,-105)
+
+fig.colorbar(surf, shrink=0.4, aspect=20)
+
+plt.show()
 
 '''
 
@@ -39,6 +66,8 @@ dataset.FlushCache()
 del dataset
 
 
+
+'''
 
 '''
 
@@ -76,6 +105,8 @@ dataset.FlushCache()
 del dataset
 del dataset2
 
+'''
+
 
 '''
 dataset : gdal.Dataset = gdal.Open('data/raster/HELLO.tiff', gdal.GA_Update)
@@ -83,3 +114,4 @@ memdriver : ogr.Driver = ogr.GetDriverByName('Memory')
 memdriver.CreateDataSource('')
 
 '''
+
